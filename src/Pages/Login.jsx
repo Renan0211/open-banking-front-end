c58dcdcbf1517d16ba9d6dd29886a80656e51f01
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-25 18:24:56 -0300

    implements the login function to the login page, using the API that i have developed for this project

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,27 +1,45 @@
-import React, { Component } from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Container, FloatingLabel, Form, Button } from 'react-bootstrap'
+import { Redirect } from 'react-router';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div className="d-flex flex-column justify-content-center">
-        <h1 className="text-light text-center mb-5">Login</h1>
-        <Container className="">
-          <Form className="align-middle d-flex flex-column align-items-center">
-            <FloatingLabel
-              controlId="email"
-              label="Email address"
-              className="mb-3 w-50"
-            >
-              <Form.Control type="email" placeholder="name@example.com" />
-            </FloatingLabel>
-            <FloatingLabel controlId="password" label="Password" className="w-50">
-              <Form.Control type="password" placeholder="Password" />
-            </FloatingLabel>
-            <Button className="my-2 mx-auto">Login</Button>
-          </Form>
-        </Container>
-      </div>
-    )
+export default function Login (){
+  const [userData, setUserData] = useState({});
+  const [redirect, setRedirect] = useState('');
+
+  const fetchLogin = async () => {
+    await axios({
+      method: 'POST',
+      baseURL: 'https://dev-test-4cadia--renan0211.herokuapp.com/',
+      url: '/login',
+      data: userData,
+    }).then(res => {
+      localStorage.setItem('token', res.data.token);
+      setRedirect('user/balance')
+    }).catch(e => alert(e.response.data.message));
   }
+
+  return (
+    <div className="d-flex flex-column justify-content-center">
+      {
+        redirect !== '' ? <Redirect to={`/${redirect}`}/> : ''
+      }
+      <h1 className="text-light text-center mb-5">Login</h1>
+      <Container className="">
+        <Form className="align-middle d-flex flex-column align-items-center">
+          <FloatingLabel
+            controlId="email"
+            label="Email address"
+            className="mb-3 w-50"
+          >
+            <Form.Control type="email" placeholder="name@example.com" onChange={i => setUserData({...userData, email: i.target.value})}/>
+          </FloatingLabel>
+          <FloatingLabel controlId="password" label="Password" className="w-50">
+            <Form.Control type="password" placeholder="Password" onChange={i => setUserData({...userData, password: i.target.value})}/>
+          </FloatingLabel>
+          <Button className="my-2 mx-auto" onClick={fetchLogin}>Login</Button>
+        </Form>
+      </Container>
+    </div>
+  )
 }
